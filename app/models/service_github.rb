5b038626343fc3d5290757eb0b1c7de3c874c5cd
Gabriela Niño <gabynr@gmail.com>
2021-02-12 19:54:50 -0500

    #aplied Rubocop to ServiceGithub

--- app/models/service_github.rb
+++ app/models/service_github.rb
@@ -1,14 +1,12 @@
 class ServiceGithub
-
   def initialize
-    @client = Octokit::Client.new(:access_token => ENV['GITHUB_SECRET'])
-
+    @client = Octokit::Client.new(access_token: ENV['GITHUB_SECRET'])
   end
 
   def new_issues
-    list_author = Author.select('authors.name as name, authors.id as id' ).where(github_issue_id: nil)
+    list_author = Author.select('authors.name as name, authors.id as id').where(github_issue_id: nil)
     list_author.each do |l|
-      new_issue= @client.create_issue('gabynr/bookstore4', l.name)
+      new_issue = @client.create_issue('gabynr/bookstore4', l.name)
       author = Author.find_by(id: l.id)
       author.update(github_issue_id: new_issue.number)
     end
