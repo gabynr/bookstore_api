8416bc5df74131dd9cfda034475a133155468783
Gabriela Niño <gabynr@gmail.com>
2021-02-12 22:35:32 -0500

    refactor GithubAuthorSyncService

--- app/models/github_author_sync_service.rb
+++ app/models/github_author_sync_service.rb
@@ -6,25 +6,58 @@ class GithubAuthorSyncService
   end
 
   def sync_author
-    if payload['action'] == 'opened' || (
-    payload['action'] == 'edited' &&
-        Author.find_by(github_issue_id: payload['issue']['number']).nil?
-    )
-      if Author.find_by(github_issue_id: payload['issue']['number']).nil?
-        author = Author.create(
-          github_issue_id: payload['issue']['number'],
-          name: payload['issue']['title'],
-          bio: payload['issue']['body']
-        )
-        Book.create(title: 'El primer libro maravilloso', price: 10, author: author, publisher: author)
-      end
-    elsif payload['action'] == 'edited'
-      author = Author.find_by github_issue_id: payload['issue']['number']
-      author.update_attributes name: payload['issue']['title'], bio: payload['issue']['body']
-    elsif payload['action'] == 'deleted'
-      author = Author.find_by github_issue_id: payload['issue']['number']
-      author.books.each(&:destroy)
-      author.destroy
+    case action
+    when 'opened'
+      handle_opened
+    when 'edited'
+      handle_edited
+    when 'deleted'
+      handle_deleted
     end
+  end
+
+  private
+
+  def handle_opened
+    create_author if author.nil?
+  end
+
+  def handle_edited
+    if author.nil?
+      create_author
+    else
+      author.update_attributes name: issue_title, bio: issue_body
+    end
+  end
+
+  def handle_deleted
+    author.books.each(&:destroy)
+    author.destroy
+  end
+  FIRST_BOOK_TITLE = 'El primer libro maravilloso'.freeze
+  FIRST_BOOK_PRICE = 10
+  def create_author
+    new_author = Author.create(github_issue_id: issue_number, name: issue_title, bio: issue_body)
+    Book.create(title: FIRST_BOOK_TITLE, price: FIRST_BOOK_PRICE, author: new_author, publisher: new_author)
+  end
+
+  def issue_body
+    payload['issue']['body']
+  end
+
+  def issue_title
+    payload['issue']['title']
+  end
+
+  def issue_number
+    payload['issue']['number']
+  end
+
+  def action
+    payload['action']
+  end
+
+  def author
+    Author.find_by(github_issue_id: issue_number)
   end
 end
